056668bc074a3f4213d06a9b7f08343102c29b48
pbluc <plucdevelop@gmail.com>
2023-04-16 17:47:42 -0400

    Added API calls for review

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 var logoutBtn = document.getElementById("logoutBtn");
 var writeReviewBtn = document.getElementById("writeReviewBtn");
+var listOfReviews = document.getElementById("listOfReviews");
 
 logoutBtn.onclick = function() {logout()};
 writeReviewBtn.onclick = function() {
@@ -9,4 +10,33 @@ writeReviewBtn.onclick = function() {
 function logout() {
     console.log('Logged out')
     location.href = "index.html";
+}
+
+$(document).ready(function() {
+    getReviews();
+});
+
+function getReviews() {
+    params = {
+        "useremail": "patricia.b.luc@gmail.com",
+        "businessemail": ""
+    };
+
+    apigClient.reviewsGet(params, {}, {})
+    .then((response) => {
+        console.log(response);
+
+        response.forEach(review => {
+            let business = review['business-email'];
+            let rating = review['rating'];
+            let description = review['review'];
+            let reply = review['reply'];
+
+            var templateReviewCard = `<div class="card"><div class="container"><h4><b>${business}</b></h4><p>${rating}</p><p>${description}</p><p>${reply}</p></div></div>`;
+
+            listOfReviews.appendChild(templateReviewCard);
+        });
+    }).catch((error) => {
+        console.log('an error occurred', error);
+    });
 }
--- js/review.js
+++ js/review.js
@@ -1,11 +1,35 @@
 var closeBtn = document.getElementById("closeBtn");
-var submitReviewBtn = document.getElementById("submitReviewBtn")
+var submitReviewBtn = document.getElementById("submitReviewBtn");
+var ratingInput = document.getElementById("ratingInput");
+var descriptionInput = document.getElementById("descriptionInput");
+var businessEmailInput = document.getElementById("businessEmailInput");
+
+var apigClient = apigClientFactory.newClient();
 
 closeBtn.onclick = function() {
     location.href = "home.html";
 };
 
 submitReviewBtn.onclick = function() {
-    alert('Review was successfully submitted!');
-    location.href = "home.html";
+    body = {
+        "user-email": "patricia.b.luc@gmail.com",
+        "business-email": businessEmailInput.value,
+        "rating": ratingInput.value,
+        "review": descriptionInput.value,
+        "business-reply": ""
+    };
+
+    apigClient.reviewPut({}, body, {})
+    .then((response) => {
+        // Clear fields
+        businessEmailInput.value = "";
+        ratingInput.value = "";
+        descriptionInput.value = "";
+        // Open popup indicating success to user
+        alert('Review was successfully submitted!');
+        // Redirect to user home page
+        location.href = "home.html";
+    }).catch((error) => {
+        console.log('an error occurred', error);
+    });
 };
