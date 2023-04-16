4ed3a29ec88a9a9a8b50d55a10a01e53b9c6d31d
pbluc <plucdevelop@gmail.com>
2023-04-16 16:54:33 -0400

    Updated Amazon Cognito redirect links

--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ loginAsBusinessBtn.onclick = function() {businessLogin()};
 
 function userLogin() {
     console.log('Open Amazon Cognito as user');
-    location.href = "https://togoapp.auth.us-east-1.amazoncognito.com/login?client_id=3s5ild4hkht25elgk38ra3vv9q&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.togo-food-trucks.com.s3-website-us-east-1.amazonaws.com%2Fhome.html";
+    location.href = "https://togoapp.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=3s5ild4hkht25elgk38ra3vv9q&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd21eulsh8uxgb2.cloudfront.net%2Fhome.html";
 }
 
 function businessLogin() {
     console.log('Open Amazon Cognito as business');
-    location.href = "https://togoapp-businesses.auth.us-east-1.amazoncognito.com/login?client_id=5fu191vd265c8tme5vfh843dh1&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.togo-food-trucks.com.s3-website-us-east-1.amazonaws.com%2Fbusiness.html";
+    location.href = "https://togoapp-businesses.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=5fu191vd265c8tme5vfh843dh1&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd21eulsh8uxgb2.cloudfront.net%2Fbusiness.html";
 }
+Alt+Q
