02eeb4b6d5152eba83f714369737c65a08ab388e
pbluc <plucdevelop@gmail.com>
2023-04-17 00:21:59 -0400

    Get user email from Cognito

--- js/home.js
+++ js/home.js
@@ -6,6 +6,7 @@ var apigClient = apigClientFactory.newClient();
 
 var userEmail = "";
 
+console.log(window.location.search);
 var accessCode = window.location.search.substring(6);
 console.log(accessCode);
 
@@ -13,30 +14,39 @@ fetch('https://togoapp.auth.us-east-1.amazoncognito.com/oauth2/token', {
     method: 'POST',
     headers: {
         'Authorization': 'Basic M3M1aWxkNGhraHQyNWVsZ2szOHJhM3Z2OXE6MXBndmdpaGIzZjFhamZzazdhNmJnbDA2YXFsMDEybzc5dHJtbG83bmo1cnY1aGEydDcyNg==',
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: `grant_type=authorization_code&client_id=3s5ild4hkht25elgk38ra3vv9q&code=${accessCode}redirect_uri=https://d21eulsh8uxgb2.cloudfront.net/home.html&client_secret=1pgvgihb3f1ajfsk7a6bgl06aql012o79trmlo7nj5rv5ha2t726`
+    body: `grant_type=authorization_code&client_id=3s5ild4hkht25elgk38ra3vv9q&code=${accessCode}&redirect_uri=https://d21eulsh8uxgb2.cloudfront.net/home.html&client_secret=1pgvgihb3f1ajfsk7a6bgl06aql012o79trmlo7nj5rv5ha2t726`
 }).then(response => {
-    response = response.json;
-    var accessToken = response["access_token"];
-    console.log(accessToken);
+    response = response.json();
+    console.log(response);
+    response.then(function(result) {
+        console.log(result);
+        var accessToken = result["access_token"];
+        console.log(accessToken);
 
-    fetch('https://cognito-idp-fips.us-east-1.amazonaws.com', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-amz-json-1.1',
-            'X-Amz-Target': 'AWSCognitoIdentityProviderService.GetUser'
-        },
-        body: `{"AccessToken":"${accessToken}"}`
-    }).then(userProfile => {
-        console.log(userProfile);
-        userProfile["UserAttributes"].forEach(attr => {
-            if ("Name" in attr && attr["Name"] == "email") {
-                userEmail = attr["Value"];
-                getReviews();
-                console.log(userEmail);
-                return;
-            }
+        fetch('https://cognito-idp-fips.us-east-1.amazonaws.com', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-amz-json-1.1',
+                'X-Amz-Target': 'AWSCognitoIdentityProviderService.GetUser'
+            },
+            body: `{"AccessToken":"${accessToken}"}`
+        }).then(userProfile => {
+            console.log(userProfile);
+            userProfile = userProfile.json();
+            console.log(userProfile);
+            userProfile.then(function(profile) {
+                console.log(profile);
+                profile["UserAttributes"].forEach(attr => {
+                    if ("Name" in attr && attr["Name"] == "email") {
+                        userEmail = attr["Value"];
+                        getReviews();
+                        console.log(userEmail);
+                        return;
+                    }
+                });
+            });
         });
     });
 });
